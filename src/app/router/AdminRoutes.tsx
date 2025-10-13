898c30c7b0795d56bdc4f4e61f64c8ddb4cf1659
alexisgtierrz <alexisgutierrez1072@gmail.com>
2025-10-13 00:44:57 -0300

    Agrega la pagina cuenta del usuario en las rutas del administrador

--- src/app/router/AdminRoutes.tsx
+++ src/app/router/AdminRoutes.tsx
@@ -3,12 +3,14 @@ import DashboardPage from "../../features/dashboard/pages/DashboardPage";
 import CatalogPage from "../../features/catalog/pages/CatalogPage";
 import CreateProductForm from "../../features/catalog/components/CreateProductForm";
 import EditProductForm from "../../features/catalog/components/EditProductForm"; 
+import CuentaPage from "../../features/dashboard/pages/CuentaPage";
 
 const AdminRoutes = [
   { path: "/dashboard", element: <DashboardPage /> },
   { path: "/dashboard/catalogo", element: <CatalogPage /> },
   { path: "/dashboard/catalogo/nuevo", element: <CreateProductForm /> },
-  { path: "/dashboard/catalogo/:id/editar", element: <EditProductForm /> }, 
+  { path: "/dashboard/catalogo/:id/editar", element: <EditProductForm /> },
+  { path: "/dashboard/cuenta", element: <CuentaPage /> },
   // ...
 ];
 
